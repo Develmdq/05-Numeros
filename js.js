2c212b41cdcd4231c0cae4c02c146f1ade3f8cda
eledu <develmdq@gmail.com>
2022-06-13 22:03:38 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,10 @@
+const alturaEnCent = 170;
+const alturaEnMetros = 1.7;
+const peso = 70.5;
+const alturaRedondeada = Math.ceil(alturaEnMetros);
+const pesoRedondeado = Math.floor(peso);
+const iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
+console.log(alturaRedondeada);
+console.log(pesoRedondeado);
+console.log(iguales);
